d0c84e962b554fef250118fb703771a854e0fb57
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-05-23 12:58:59 +0300

    Initial form submit handler

---
+++ dashboard/client/dashboard.js
@@ -0,0 +1,11 @@
+Template.dashboard.events({
+  'submit #elasticsearch-host' (event, templateInstance) {
+    // prevent default form action
+    event.preventDefault();
+
+    // Get Elasticsearch host from form
+    const host = event.target.host.value;
+
+    console.log(host);
+  }
+})
