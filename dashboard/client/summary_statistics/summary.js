9e50050b503d9fa7682f37ff1fd04838e5f9d244
Daria Voytova <vdariaclem@gmail.com>
2017-07-27 18:35:07 +0300

    Get data for previous period

--- dashboard/client/summary_statistics/summary.js
+++ dashboard/client/summary_statistics/summary.js
@@ -7,15 +7,18 @@ Template.summaryStatistic.helpers({
     const buckets = elasticsearchData.aggregations.group_by_request_path.buckets;
 
     return buckets.map(value => {
+      // Get data for current period.
+      const currentPeriodBucket = value.group_by_interval.buckets['currentWeek'];
+
       return {
         // TODO: Instead of attr create an unique ID. It needs to create an unique selector for SVG chart
         attr: value.key,
         requestPath: value.key,
-        requestNumber: value.doc_count,
-        responseTime: parseInt(value.response_time.values['95.0'], 10),
-        uniqueUsers: value.unique_users.buckets.length,
-        requestOverTime: value.requests_over_time,
-        averageResponseTime: value.requests_over_time,
+        requestNumber: currentPeriodBucket.doc_count,
+        responseTime: parseInt(currentPeriodBucket.response_time.values['95.0'], 10),
+        uniqueUsers: currentPeriodBucket.unique_users.buckets.length,
+        requestOverTime: currentPeriodBucket.requests_over_time,
+        averageResponseTime: currentPeriodBucket.requests_over_time,
       }
     });
   },
